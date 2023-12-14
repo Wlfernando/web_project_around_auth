a57c29e012974d8ff79f9811cfb03111506d53d9
Fernando Alcázar Malfavón <4maferal@gmail.com>
2023-12-13 22:55:30 -0600

    Feat: add Register

---
+++ src/components/Register.jsx
@@ -0,0 +1,11 @@
+import SignWithForm from "./SignWithForm";
+
+export default function Register() {
+  return (
+    <SignWithForm
+      name="sign-up"
+      title="Regístrate"
+      para="¿Ya eres miembro? Inicia sesión"
+    />
+  )
+}
