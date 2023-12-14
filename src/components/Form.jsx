f14254dc1716be6dca75ba51d5f7208fea8ae08d
Fernando Alcázar Malfavón <4maferal@gmail.com>
2023-12-13 22:43:56 -0600

    Feat: support SignWithForm for component Form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,10 @@
 export default function Form({
   name,
   title,
+  mod,
   children,
   isDisabled,
+  para,
   onSubmit,
   onValidation,
   btn: {
@@ -19,26 +21,27 @@ export default function Form({
 
   return (
     <form
-      className="form form_popup"
+      className={`form form_type_${mod}`}
       name={name}
       noValidate
       onSubmit={handleSubmit}
     >
       <fieldset
-        className="form__fieldset"
+        className={`form__fieldset form__fieldset_type_${mod}`}
         onChange={onValidation}
       >
-        <h3 className="form__title">{title}</h3>
+        <h3 className={`form__title form__title_type_${mod}`}>{title}</h3>
         {children}
-        <button
-          type="submit"
-          className={"button button__submit" + btnOff}
-          disabled={isDisabled}
-          ref={btnRef}
-        >
-          {btnText ??= 'Guardar'}
-        </button>
       </fieldset>
+      <button
+        type="submit"
+        className={`button button__submit button__submit_type_${mod} ${btnOff}`}
+        disabled={isDisabled}
+        ref={btnRef}
+      >
+        {btnText ??= 'Guardar'}
+      </button>
+      {para && <p className="form__question">{para}</p>}
     </form>
   )
 }
