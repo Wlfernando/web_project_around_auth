68c9253a9c58df6acdddf6e12517c1e82f7a4670
Fernando Alcázar Malfavón <4maferal@gmail.com>
2023-12-13 22:54:33 -0600

    Feat: add Login

---
+++ src/components/Login.jsx
@@ -0,0 +1,11 @@
+import SignWithForm from "./SignWithForm";
+
+export default function Login() {
+  return (
+    <SignWithForm
+      name= "sign-in"
+      title= "Inicia sesión"
+      para= '¿Aún no eres miembro? Regístrate'
+    />
+  )
+}
