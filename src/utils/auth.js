a2e9ce1a52f23b85f5ef91b3a89baac8ff09c500
Fernando Alcázar Malfavón <4maferal@gmail.com>
2023-12-14 18:52:52 -0600

    Feat: add auth file

---
+++ src/utils/auth.js
@@ -0,0 +1,43 @@
+const BASE_URL = "https://register.nomoreparties.co";
+
+function confirmBody(user) {
+  try {
+    if (typeof user !== 'object') {
+      throw new Error('Pass an object')
+    }
+
+    if (!user.password) {
+      throw new Error('Missed password')
+    }
+
+    if (!user.email) {
+      throw new Error('Missed email')
+    }
+
+    if (Object.keys(user).length !== 2) {
+      throw new Error('There\'re more elements needed')
+    }
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+function handleOpt(sendBody) {
+  return {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(sendBody)
+  }
+}
+
+export function register(user) {
+  confirmBody(user)
+
+  return fetch(`${BASE_URL}/signup`, handleOpt(user))
+    .then(res => {
+      if (res.ok) return res.json()
+      return Promise.reject(res)
+    })
+}
